c8a9f6a4564dab7c4f64d2109a27167a040ef128
Samira Esmail Pur <samiraesmailpur05@gmail.com>
2023-05-01 02:50:13 +0300

    added path

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/test-goit-tweets">
       <App />
     </BrowserRouter>
   </React.StrictMode>
